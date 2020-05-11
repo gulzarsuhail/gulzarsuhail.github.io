88f2faadb595a9d4331c3f056d70297b71a8391d
gulzarsuhail <suhail.95cse14@mietjammu.in>
2020-05-11 22:15:06 +0530

    internet explorer critical compatibility fix

---
+++ precache-manifest.46d13e6a0f2654b5e5a4af378f220981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27a393bf350752dbd5746e691fdf346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2616e9201602eac96d1f",
+    "url": "/static/css/main.4276585b.chunk.css"
+  },
+  {
+    "revision": "a141c1e7336574a757fc",
+    "url": "/static/js/2.9c475c87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c475c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2616e9201602eac96d1f",
+    "url": "/static/js/main.0765d19d.chunk.js"
+  },
+  {
+    "revision": "19d6cd8c4707d3ac4e75",
+    "url": "/static/js/runtime-main.1b29f7f1.js"
+  }
+]);
