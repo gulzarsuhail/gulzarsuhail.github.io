61624477a60cb418a0d175374de6204a01b3db49
gulzarsuhail <suhail.gulzar95@gmail.com>
2021-05-21 01:08:08 +0530

    update resume

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ee40d2e2434e27a0ae05df57aec5a13.js"
+  "/precache-manifest.dc9973242707ea1cf6525d54c7f32108.js"
 );
 
 self.addEventListener('message', (event) => {
